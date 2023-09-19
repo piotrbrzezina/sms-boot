057e02adfca0498ef898a2ef56ea1139254ec5be
marcin <marcin@fratczak.pl>
2023-09-19 16:21:34 +0200

    Ui adjust

--- src/app/form.tsx
+++ src/app/form.tsx
@@ -6,15 +6,17 @@ export default function Form() {
 	const [message, setMessage] = useState("");
 	const [url, setUrl] = useState("");
 	const [userId, setUserId] = useState("");
+	const [isActive, setIsActive] = useState(false);
 
 	if (url === "") {
 		setUrl("https://d541-91-233-251-217.ngrok-free.app/api/chat");
 		setUserId(uuidv4());
+		setIsActive(false);
 	}
 
 	async function onSubmit(event: FormEvent<HTMLFormElement>) {
 		event.preventDefault();
-		// https://d541-91-233-251-217.ngrok-free.app/api/chat
+
 		const formData = new FormData(event.currentTarget);
 		const request = { message: formData.get("message"), user_id: formData.get("user_id") };
 		const url = formData.get("url");
@@ -35,7 +37,7 @@ export default function Form() {
 			`;
 
 			setMessage("");
-
+			setIsActive(true);
 			commentsList?.append(comment);
 			const response = await fetch(url.toString(), {
 				method: "POST",
@@ -63,49 +65,53 @@ export default function Form() {
 			</div>
 
 			`;
-
+				setIsActive(false);
 				commentsList?.append(commentResponse);
 			}
 		}
 	}
 
 	return (
-		<div className="p-15 bg-gray-100 pl-0">
-			<form onSubmit={onSubmit}>
-				<p className="p-2">
-					<label>Message:</label>
-					<input
-						type="text"
-						name="message"
-						className="w-9/12"
-						id="message"
-						value={message}
-						onChange={(e) => setMessage(e.target.value)}
-					/>
-				</p>
+		<div>
+			<div id="spinner" className={`lds-dual-ring ${isActive ? "" : "invisible"}`}></div>
 
-				<p className="p-2">
-					<label>Url:</label>
-					<input
-						type="text"
-						name="url"
-						id="url"
-						className="w-6/12"
-						value={url}
-						onChange={(e) => setUrl(e.target.value)}
-					/>
-				</p>
-				<input type="hidden" name="user_id" value={userId} />
+			<div className="p-15 bg-gray-100 pl-0">
+				<form onSubmit={onSubmit}>
+					<p className="p-2">
+						<label>Message:</label>
+						<input
+							type="text"
+							name="message"
+							className="w-9/12"
+							id="message"
+							value={message}
+							onChange={(e) => setMessage(e.target.value)}
+						/>
+					</p>
 
-				<p className="pt-2">
-					<button
-						type="submit"
-						className="mb-1 mr-1 rounded border border-gray-100 px-4 py-2 text-xs font-bold uppercase text-gray-800 outline-none transition-all duration-150 ease-linear hover:bg-green-950 hover:text-white focus:outline-none "
-					>
-						Send message &raquo;
-					</button>
-				</p>
-			</form>
+					<p className="p-2">
+						<label>Url:</label>
+						<input
+							type="text"
+							name="url"
+							id="url"
+							className="w-6/12"
+							value={url}
+							onChange={(e) => setUrl(e.target.value)}
+						/>
+					</p>
+					<input type="hidden" name="user_id" value={userId} />
+
+					<p className="pt-2">
+						<button
+							type="submit"
+							className="mb-1 mr-1 rounded border border-gray-100 px-4 py-2 text-xs font-bold uppercase text-gray-800 outline-none transition-all duration-150 ease-linear hover:bg-green-950 hover:text-white focus:outline-none "
+						>
+							Send message &raquo;
+						</button>
+					</p>
+				</form>
+			</div>
 		</div>
 	);
 }
